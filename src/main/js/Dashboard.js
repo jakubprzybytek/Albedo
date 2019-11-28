e5eea1d1e76ea3288abbaa9d5ec7c2673c3c937d
Jakub Przybytek <jakub.przybytek@gmail.com>
2019-11-28 23:25:10 +0100

    Small refactor

--- src/main/js/Dashboard.js
+++ src/main/js/Dashboard.js
@@ -1,29 +1,20 @@
 import React from 'react';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
+import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import SettingsIcon from '@material-ui/icons/Settings';
 import PersonIcon from '@material-ui/icons/Person';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import EventsList from './eventslist/EventsList';
 import ToolTabs from './ToolTabs';
 import UserDrawer from './UserDrawer';
+import SettingsDrawer from './SettingsDrawer';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -72,13 +63,6 @@ const useStyles = makeStyles(theme => ({
   hide: {
     display: 'none',
   },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
   drawerHeader: {
     display: 'flex',
     minHeight: 48,
@@ -105,19 +89,10 @@ const useStyles = makeStyles(theme => ({
 
 export default function PersistentDrawerLeft() {
   const classes = useStyles();
-  const theme = useTheme();
 
   const [tabValue, setTabValue] = React.useState(0);
+  const [settingsDrawerOpened, setSettingsDrawerOpened] = React.useState(true);
   const [userDrawerOpened, setUserDrawerOpened] = React.useState(false);
-  const [open, setOpen] = React.useState(false);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
 
   const toggleDrawer = (open) => event => {
     if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
@@ -129,9 +104,9 @@ export default function PersistentDrawerLeft() {
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar position="fixed" className={clsx(classes.appBar, { [classes.appBarShift]: open, })} color="default" >
+      <AppBar position="fixed" className={clsx(classes.appBar, { [classes.appBarShift]: settingsDrawerOpened, })} color="default" >
         <Toolbar className={classes.toolBar}>
-          <IconButton color="inherit" aria-label="open drawer" onClick={handleDrawerOpen} edge="start" className={clsx(classes.menuButton, open && classes.hide)} >
+          <IconButton color="inherit" onClick={() => setSettingsDrawerOpened(true)} edge="start" className={clsx(classes.menuButton, settingsDrawerOpened && classes.hide)} >
             <SettingsIcon />
           </IconButton>
         </Toolbar>
@@ -145,16 +120,8 @@ export default function PersistentDrawerLeft() {
           </IconButton>
         </Toolbar>
       </AppBar>
-      <Drawer className={classes.drawer} variant="persistent" anchor="left" open={open} classes={{ paper: classes.drawerPaper, }} >
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-          </IconButton>
-        </div>
-        <Divider />
-        Hello world
-      </Drawer>
-      <main className={clsx(classes.content, { [classes.contentShift]: open, })} >
+      <SettingsDrawer opened={settingsDrawerOpened} setOpened={setSettingsDrawerOpened} />
+      <main className={clsx(classes.content, { [classes.contentShift]: settingsDrawerOpened, })} >
         <div className={classes.drawerHeader} />
         <TabPanel value={tabValue} index={0}>
           <EventsList />
