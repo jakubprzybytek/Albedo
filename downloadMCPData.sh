ccc47a0ba70303cfb48e5895d6743b205159b28b
Jakub Przybytek <jakub.przybytek@gmail.com>
2020-11-01 22:29:30 +0100

    Downloading and docker volume update script

--- downloadMCPData.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo Downloading data from Minor Planet Center
-
-# curl https://www.minorplanetcenter.net/iau/MPCORB/MPCORB.DAT.gz --output MPCORB.DAT.gz
-curl https://www.minorplanetcenter.net/iau/MPCORB/CometEls.txt --output CometEls.txt
