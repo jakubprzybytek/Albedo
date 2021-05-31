220e241c7343542d21504358d6aa543adf80361e
Jakub Przybytek <jakub.przybytek@gmail.com>
2021-05-31 22:45:54 +0200

    CDK removed

--- cdk/bin/cdk.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { AlbedoBuildStack } from '../lib/albedo-build-stack';
-
-const app = new cdk.App();
-new AlbedoBuildStack(app, 'AlbedoBuildStack', {
-  env: { account: '198805281865', region: 'eu-west-1' },
-});
