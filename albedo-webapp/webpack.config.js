3853f7481bce74bd5ef1fd0682f476586f221354
Jakub Przybytek <jakub.przybytek@gmail.com>
2021-06-08 22:48:41 +0200

    Some upgrades

--- albedo-webapp/webpack.config.js
+++ albedo-webapp/webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
     entry: './src/main/js/app.js',
-    devtool: 'sourcemaps',
+    devtool: 'source-map',
     devServer: {
      contentBase: './src/main/resources/static',
      proxy: {
