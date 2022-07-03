e0d0858ff322761926cd693efcff8a37152f0dd7
Jakub Przybytek <jakub.przybytek@gmail.com>
2022-07-03 16:50:30 +0200

    Initial responsivness

--- frontend/components/States/StatesBrowser.tsx
+++ frontend/components/States/StatesBrowser.tsx
@@ -7,9 +7,13 @@ export default function StatesBrowser(): JSX.Element {
     const [states, setStates] = useState<GetStatesReturnType>([]);
 
     return (
-        <div>
-            <StatesQueryForm setStates={setStates} />
-            <StatesTable states={states} />
-        </div>
+        <>
+            <div>
+                <StatesQueryForm setStates={setStates} />
+            </div>
+            <div>
+                <StatesTable states={states} />
+            </div>
+        </>
     );
 }
--- frontend/components/States/StatesQueryForm.tsx
+++ frontend/components/States/StatesQueryForm.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { GetStatesReturnType } from '@lambda/states/getStates';
+import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import styles from "./StatesQueryForm.module.css";
 
 type StatesQueryFormParams = {
     setStates: (states: GetStatesReturnType) => void;
@@ -16,6 +18,8 @@ export default function StatesQueryForm({ setStates }: StatesQueryFormParams): J
     const [toTde, setToTde] = useState<Date | null>(new Date('2019-10-12'));
     const [interval, setInterval] = useState('1');
 
+    const [loading, setLoading] = useState(false);
+
     function handleSubmit() {
         const params = new URLSearchParams({
             target,
@@ -25,53 +29,70 @@ export default function StatesQueryForm({ setStates }: StatesQueryFormParams): J
             interval
         });
 
+        setLoading(true);
+
         fetch(process.env.NEXT_PUBLIC_API_URL + '/api/states?' + params.toString())
             .then((response) => response.json())
             .then((data) => {
                 console.log('Fetched: ' + data);
                 setStates(data);
+                setLoading(false);
             });
     }
 
     return (
-        <Box component="form" sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' }, }}>
-            <div>
-                <TextField label="Target" size="small"
-                    value={target}
-                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                        setTarget(event.target.value);
-                    }}
-                />
-                <TextField label="Observer" size="small"
-                    value={observer}
-                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                        setObserver(event.target.value);
-                    }}
-                />
-                <DatePicker label="From (TDE)"
-                    renderInput={(params) => <TextField size="small" {...params} />}
-                    value={fromTde}
-                    onChange={(newValue) => {
-                        setFromTde(newValue);
-                    }}
-                />
-                <DatePicker label="To  (TDE)"
-                    renderInput={(params) => <TextField size="small" {...params} />}
-                    value={toTde}
-                    onChange={(newValue) => {
-                        setToTde(newValue);
-                    }}
-                />
-                <TextField label="Interval" size="small" type="number"
-                    value={interval}
-                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                        setInterval(event.target.value);
-                    }}
-                />
-                <Button variant="contained" size="small"
-                    onClick={handleSubmit}
-                >Submit</Button>
-            </div>
+        <Box component="form">
+            <Grid container>
+                <Grid item xs={12} sm={6}>
+                    <TextField label="Target" size="small" className={styles.field}
+                        value={target}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                            setTarget(event.target.value);
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField label="Observer" size="small" className={styles.field}
+                        value={observer}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                            setObserver(event.target.value);
+                        }}
+                    />
+                </Grid>
+            </Grid>
+            <Grid container>
+                <Grid item xs={12} sm={4}>
+                    <DatePicker label="From (TDE)"
+                        renderInput={(params) => <TextField size="small" className={styles.field} {...params} />}
+                        value={fromTde}
+                        onChange={(newValue) => {
+                            setFromTde(newValue);
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <DatePicker label="To (TDE)"
+                        renderInput={(params) => <TextField size="small" className={styles.field} {...params} />}
+                        value={toTde}
+                        onChange={(newValue) => {
+                            setToTde(newValue);
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <TextField label="Interval" size="small" type="number" className={styles.field}
+                        value={interval}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                            setInterval(event.target.value);
+                        }}
+                    />
+                </Grid>
+                <Grid item>
+                    <Button variant="contained" size="small" disabled={loading}
+                        onClick={handleSubmit}
+                    >Submit</Button>
+                </Grid>
+            </Grid>
         </Box>
     );
 }
