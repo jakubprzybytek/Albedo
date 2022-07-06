bf01d49d8c36c6ea54f3536a81490240f7cf85ca
Jakub Przybytek <jakub.przybytek@gmail.com>
2022-07-06 22:41:04 +0200

    Form functionality extracted

---
+++ frontend/components/QueryForm.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { Auth, API } from "aws-amplify";
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import LoadingButton from '@mui/lab/LoadingButton';
+
+type QueryFormParams = {
+    path: string;
+    params: { [key: string]: string };
+    setData: (data: any) => void;
+    children: JSX.Element;
+};
+
+export default function QueryForm({ path, params, setData, children }: QueryFormParams): JSX.Element {
+
+    const [loading, setLoading] = useState(false);
+    const [successMessage, setSuccessMessage] = useState<string | null>();
+    const [errorMessage, setErrorMessage] = useState<string | null>();
+
+    async function handleSubmit() {
+        setLoading(true);
+
+        const startTime = new Date().getTime();
+
+        API.get('api', path + '?' + new URLSearchParams(params).toString(), {
+            headers: {
+                Authorization: `Bearer ${(await Auth.currentSession())
+                    .getAccessToken()
+                    .getJwtToken()}`,
+            },
+        })
+            .then((data) => {
+                console.log('Fetched: ' + data);
+                setSuccessMessage(`Loaded in ${new Date().getTime() - startTime} ms`);
+                setErrorMessage(null);
+                setData(data);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.log(error);
+                setSuccessMessage(null);
+                setErrorMessage(error.toString());
+                setLoading(false);
+            });
+    }
+
+    return (
+        <Paper component="form" sx={{
+            pt: 2, pl: 1,
+            maxWidth: '800px',
+            '& .MuiGrid-item': { pb: 1, pr: 1 },
+            '& .MuiTextField-root': { width: '100%' }
+        }}>
+            {children}
+            <Grid container sx={{ justifyContent: 'space-between' }}>
+                <Grid item>
+                    {errorMessage && <Typography sx={{ color: 'red' }}>{errorMessage}</Typography>}
+                    {successMessage && <Typography>{successMessage}</Typography>}
+                </Grid>
+                <Grid item>
+                    <LoadingButton variant="contained" size="small" loading={loading}
+                        onClick={handleSubmit}
+                    >Submit</LoadingButton>
+                </Grid>
+            </Grid>
+        </Paper>
+    );
+}
--- frontend/components/States/StatesQueryForm.tsx
+++ frontend/components/States/StatesQueryForm.tsx
@@ -1,12 +1,9 @@
 import { useState } from "react";
-import { Auth, API } from "aws-amplify";
 import { GetStatesReturnType } from '@lambda/states/getStates';
 import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
-import LoadingButton from '@mui/lab/LoadingButton';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import QueryForm from '../QueryForm';
 
 type StatesQueryFormParams = {
     setStates: (states: GetStatesReturnType) => void;
@@ -19,112 +16,64 @@ export default function StatesQueryForm({ setStates }: StatesQueryFormParams): J
     const [toTde, setToTde] = useState<Date | null>(new Date('2019-10-12'));
     const [interval, setInterval] = useState('1');
 
-    const [loading, setLoading] = useState(false);
-    const [successMessage, setSuccessMessage] = useState<string | null>();
-    const [errorMessage, setErrorMessage] = useState<string | null>();
-
-    async function handleSubmit() {
-        const params = new URLSearchParams({
-            target,
-            observer,
-            fromTde: fromTde?.toISOString() || '',
-            toTde: toTde?.toISOString() || '',
-            interval
-        });
-
-        setLoading(true);
-
-        API.get('api', '/api/states?' + params.toString(), {
-            target,
-            observer,
-            fromTde: fromTde?.toISOString() || '',
-            toTde: toTde?.toISOString() || '',
-            interval,
-            headers: {
-                Authorization: `Bearer ${(await Auth.currentSession())
-                    .getAccessToken()
-                    .getJwtToken()}`,
-            },
-        })
-            .then((data) => {
-                console.log('Fetched: ' + data);
-                setSuccessMessage('Loaded');
-                setErrorMessage(null);
-                setStates(data);
-                setLoading(false);
-            })
-            .catch(error => {
-                console.log(error);
-                setSuccessMessage(null);
-                setErrorMessage(error.toString());
-                setLoading(false);
-            });
-    }
+    const params = {
+        target,
+        observer,
+        fromTde: fromTde?.toISOString() || '',
+        toTde: toTde?.toISOString() || '',
+        interval
+    };
 
     return (
-        <Paper component="form" sx={{
-            pt: 2, pl: 1,
-            maxWidth: '800px',
-            '& .MuiGrid-item': { pb: 1, pr: 1 },
-            '& .MuiTextField-root': { width: '100%' }
-        }}>
-            <Grid container>
-                <Grid item xs={12} sm={6}>
-                    <TextField label="Target" size="small"
-                        value={target}
-                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                            setTarget(event.target.value);
-                        }}
-                    />
+        <QueryForm path='/api/states' params={params} setData={setStates}>
+            <>
+                <Grid container>
+                    <Grid item xs={12} sm={6}>
+                        <TextField label="Target" size="small"
+                            value={target}
+                            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                                setTarget(event.target.value);
+                            }}
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField label="Observer" size="small"
+                            value={observer}
+                            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                                setObserver(event.target.value);
+                            }}
+                        />
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField label="Observer" size="small"
-                        value={observer}
-                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                            setObserver(event.target.value);
-                        }}
-                    />
+                <Grid container>
+                    <Grid item xs={12} sm={4}>
+                        <DatePicker label="From (TDE)"
+                            renderInput={(params) => <TextField size="small" {...params} />}
+                            value={fromTde}
+                            onChange={(newValue) => {
+                                setFromTde(newValue);
+                            }}
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <DatePicker label="To (TDE)"
+                            renderInput={(params) => <TextField size="small" {...params} />}
+                            value={toTde}
+                            onChange={(newValue) => {
+                                setToTde(newValue);
+                            }}
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={4} >
+                        <TextField label="Interval" size="small" type="number"
+                            value={interval}
+                            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                                setInterval(event.target.value);
+                            }}
+                        />
+                    </Grid>
                 </Grid>
-            </Grid>
-            <Grid container>
-                <Grid item xs={12} sm={4}>
-                    <DatePicker label="From (TDE)"
-                        renderInput={(params) => <TextField size="small" />}
-                        value={fromTde}
-                        onChange={(newValue) => {
-                            setFromTde(newValue);
-                        }}
-                    />
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                    <DatePicker label="To (TDE)"
-                        renderInput={(params) => <TextField size="small" />}
-                        value={toTde}
-                        onChange={(newValue) => {
-                            setToTde(newValue);
-                        }}
-                    />
-                </Grid>
-                <Grid item xs={12} sm={4} >
-                    <TextField label="Interval" size="small" type="number"
-                        value={interval}
-                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                            setInterval(event.target.value);
-                        }}
-                    />
-                </Grid>
-            </Grid>
-            <Grid container sx={{ justifyContent: 'space-between' }}>
-                <Grid item>
-                    {errorMessage && <Typography sx={{ color: 'red' }}>{errorMessage}</Typography>}
-                    {successMessage && <Typography>{successMessage}</Typography>}
-                </Grid>
-                <Grid item>
-                    <LoadingButton variant="contained" size="small" loading={loading}
-                        onClick={handleSubmit}
-                    >Submit</LoadingButton>
-                </Grid>
-            </Grid>
-        </Paper>
+            </>
+        </QueryForm>
     );
 }
