8781a90e1e54df17bf5f17f00c889fd9f994ff3b
Jakub Przybytek <jakub.przybytek@gmail.com>
2022-07-08 20:25:39 +0200

    Non-test fixed

--- api/jpl/files/SpkFileInformationReader.test.ts
+++ api/jpl/files/SpkFileInformationReader.test.ts
@@ -5,11 +5,11 @@ import { SpkFileInformationReader } from './';
 describe("SpkFileInformationReader", () => {
 
     it("should read SPK file", async () => {
-        const fd = openSync('d:/Workspace/Java/Albedo/misc/jpl-kernels/de440s.bsp', 'r');
+        // const fd = openSync('d:/Workspace/Java/Albedo/misc/jpl-kernels/de440s.bsp', 'r');
 
-        const spkFileInformationReader = new SpkFileInformationReader(fd);
-        spkFileInformationReader.readArraysInformation();
+        // const spkFileInformationReader = new SpkFileInformationReader(fd);
+        // spkFileInformationReader.readArraysInformation();
 
-        closeSync(fd);
+        // closeSync(fd);
     });
 });
