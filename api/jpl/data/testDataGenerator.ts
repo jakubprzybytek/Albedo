c898129d8f3b4571971c869b572e90e6fe53961a
jakub.przybytek <jakub.przybytek@wundermanthompson.com>
2022-07-20 09:25:17 +0200

    Better test data generation

--- api/jpl/data/testDataGenerator.ts
+++ api/jpl/data/testDataGenerator.ts
@@ -4,6 +4,13 @@ import { JplBodyId, EphemerisSeconds } from "..";
 import { SpkKernelCollection, TimeSpan } from '../kernel';
 import { SpkFileArrayInformation, readSpkFileInformation, readSpkPositionChebyshevPolynomials, DataType } from "../files";
 
+
+type BodiesPair = {
+    body: JplBodyId;
+    centerBody: JplBodyId;
+}
+
+
 function readRecords(fd: number, spkFileArrayInformationList: SpkFileArrayInformation[], bodyId: JplBodyId, centerBodyId: JplBodyId, fromJde: number, toJde: number): SpkKernelCollection {
     const arrayInformation = spkFileArrayInformationList
         .find(arrayInformation => arrayInformation.body.id === bodyId && arrayInformation.centerBody.id === centerBodyId);
@@ -25,6 +32,20 @@ function readRecords(fd: number, spkFileArrayInformationList: SpkFileArrayInform
     throw Error(`Unsupported data typ ${arrayInformation.dataType}`);
 }
 
+export function readMultipleSpkCollections(spkFileName: string, from: Date, to: Date, bodies: BodiesPair[]): SpkKernelCollection[] {
+    const fromJde = JulianDay.fromDateObject(from);
+    const toJde = JulianDay.fromDateObject(to);
+
+    const spkFd = openSync(spkFileName, 'r');
+    const { spkFileArrayInformationList } = readSpkFileInformation(spkFd);
+
+    const spkCollectionsList = bodies.map(pair => readRecords(spkFd, spkFileArrayInformationList, pair.body, pair.centerBody, fromJde, toJde))
+
+    closeSync(spkFd);
+
+    return spkCollectionsList;
+}
+
 function replacer(this: any, key: string, value: any): any {
     if (key === 'timeSpan') {
         const timeSpan = value as TimeSpan;
@@ -33,44 +54,29 @@ function replacer(this: any, key: string, value: any): any {
     return value;
 }
 
-function generateSpkCollection(spkFd: number, outputFd: number, spkFileArrayInformationList: SpkFileArrayInformation[], constName: string, bodyId: JplBodyId, centerBodyId: JplBodyId, fromJde: number, toJde: number) {
-    const spkCollection = readRecords(spkFd, spkFileArrayInformationList, bodyId, centerBodyId, fromJde, toJde);
-    let spkCollectionString = JSON.stringify(spkCollection, replacer, 2);
-    spkCollectionString = spkCollectionString.replaceAll(/"timeSpan": "(.+)",/g, (match, p1) => (`"timeSpan": ${p1},`));
-
-    writeSync(outputFd, `export const ${constName}: SpkKernelCollection = `);
-    writeSync(outputFd, spkCollectionString);
-    writeSync(outputFd, ';\n\n');
-}
-
-type BodiesPair = {
-    body: JplBodyId;
-    centerBody: JplBodyId;
-}
-
-export function generateTestData(spkFileName: string, outputFileName: string, from: Date, to: Date, bodiesPairs: BodiesPair[]): void {
-    const fromJde = JulianDay.fromDateObject(from);
-    const toJde = JulianDay.fromDateObject(to);
-
-    const spkFd = openSync(spkFileName, 'r');
-    const { spkFileArrayInformationList } = readSpkFileInformation(spkFd);
-
-    const outputFd = openSync(outputFileName, 'w');
-    writeSync(outputFd, "import { SpkKernelRepository, SpkKernelCollection, TimeSpan } from '../kernel';\n\n");
-    writeSync(outputFd, `// from: ${from.toISOString()}\n`);
-    writeSync(outputFd, `// to: ${to.toISOString()}\n\n`);
-
+export function printSpkCollections(outputFileName: string, spkCollectionsList: SpkKernelCollection[], from: Date, to: Date) {
+    const fd = openSync(outputFileName, 'w');
+    writeSync(fd, "import { SpkKernelRepository, SpkKernelCollection, TimeSpan } from '../kernel';\n\n");
+    writeSync(fd, `// from: ${from.toISOString()}\n`);
+    writeSync(fd, `// to: ${to.toISOString()}\n\n`);
+    
     var kernelRepositorySnippet = 'export const kernelRepository: SpkKernelRepository = new SpkKernelRepository();\n'
         + 'kernelRepository.registerSpkKernelCollections([\n';
 
-    bodiesPairs.forEach(pair => {
-        const fieldName = `a_${pair.body}_wrt_${pair.centerBody}`;
-        generateSpkCollection(spkFd, outputFd, spkFileArrayInformationList, fieldName, pair.body, pair.centerBody, fromJde, toJde);
-        kernelRepositorySnippet += `\t${fieldName},\n`;
-    });
-    kernelRepositorySnippet += ']);\n';
-    writeSync(outputFd, kernelRepositorySnippet);
+    spkCollectionsList.forEach(spkCollection => {
+        const constName = `a_${spkCollection.bodyId}_wrt_${spkCollection.centerBodyId}`;
+        let spkCollectionString = JSON.stringify(spkCollection, replacer, 2);
+        spkCollectionString = spkCollectionString.replaceAll(/"timeSpan": "(.+)",/g, (match, p1) => (`"timeSpan": ${p1},`));
 
-    closeSync(outputFd);
-    closeSync(spkFd);
+        kernelRepositorySnippet += `\t${constName},\n`;
+
+        writeSync(fd, `export const ${constName}: SpkKernelCollection = `);
+        writeSync(fd, spkCollectionString);
+        writeSync(fd, ';\n\n');
+    });
+
+    kernelRepositorySnippet += ']);\n';
+    writeSync(fd, kernelRepositorySnippet);
+
+    closeSync(fd);
 }
