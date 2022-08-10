f8cdafe8966afc4a26c530900770659c4a3f3a56
Jakub Przybytek <cubanan@gmail.com>
2022-08-11 00:46:17 +0200

    Update EventsBrowser.tsx

--- frontend/components/Events/EventsBrowser.tsx
+++ frontend/components/Events/EventsBrowser.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import EventsQueryForm from './EventsQueryForm';
 import EventsList from './EventsList';
 import getConjunctions from "./GetConjunctions";
 
