9fe2b652a2d842ec9379bc2d18f8eb4e530e9ffb
Jakub Przybytek <jakub.przybytek@vml.com>
2025-06-18 09:02:21 +0200

    Comparing old and new state solver next to each other

--- packages/core/src/jpl/test/suites/states/fullTest.states.ts
+++ packages/core/src/jpl/test/suites/states/fullTest.states.ts
@@ -9,11 +9,11 @@ import { buildStateTestCaseRunner } from './StateTestCasesScript';
 import { buildReportWriter, findFiles, ReportWriter } from "@jpl/test/lib/Files";
 import { formatDistanceStrict } from "date-fns";
 
-async function testSuite(testCaseFileNames: string[], writer: ReportWriter, stateTestCaseRunner: StateTestCaseRunner, description: string) {
+async function testSuite(testCaseFileNames: string[], writer: ReportWriter, stateTestCaseRunner: StateTestCaseRunner, state2TestCaseRunner: StateTestCaseRunner, description: string) {
   writer(`## ${description}\n`);
 
-  writer('| Target body | Observer body | Test cases | Time span | Interval | Avg postion error [km] | Avg velocity error [km/s] | File name | Kernels |');
-  writer('| ----------- | ------------- | ---------- | --------- | -------- | ---------------------- | ------------------------- | --------- | ------- |');
+  writer('| Target body | Observer body | Test cases | Time span | Interval | Avg postion error [km] | Avg velocity error [km/s] | Avg postion error - new [km] | Avg velocity error - new [km/s] | File name | Kernels |');
+  writer('| ----------- | ------------- | ---------- | --------- | -------- | ---------------------- | ------------------------- | ---------------------- | ------------------------- | --------- | ------- |');
 
   for (const testFileName of testCaseFileNames) {
     const fileContent = readFileSync(testFileName).toString();
@@ -37,10 +37,16 @@ async function testSuite(testCaseFileNames: string[], writer: ReportWriter, stat
     const positionSummary = stats.positionDifferenceAverage?.toPrecision(4) || stats.positionComputationError;
     const velocitySummary = stats.velocityDifferenceAverage?.toPrecision(4) || stats.velocityComputationError;
 
+    const stats2 = state2TestCaseRunner(targetBodyId, observerBodyId, data);
+
+    const positionSummary2 = stats2.positionDifferenceAverage?.toPrecision(4) || stats2.positionComputationError;
+    const velocitySummary2 = stats2.velocityDifferenceAverage?.toPrecision(4) || stats2.velocityComputationError;
+
     var fileName = /[^/]*$/.exec(testFileName)?.[0] || testFileName;
 
     writer(`| ${targetBodyName} | ${observerBodyName} | ${data.length} | ${timeSpan} | ${timeInterval}`
       + ` | ${positionSummary} | ${velocitySummary}`
+      + ` | ${positionSummary2} | ${velocitySummary2}`
       + ` | ${fileName} | ${kernels.join(', ')} |`);
   }
 }
@@ -54,33 +60,20 @@ export async function runStatesTestSuite(timestamp: string) {
     findFiles(path.join(__dirname, 'data/states-reference-uncorrected'), 'WGC_StateVector'),
     append,
     buildStateTestCaseRunner({ corrections: [] }),
+    runState2TestCases,
     'State without correction'
   );
   await testSuite(findFiles(path.join(__dirname, 'data/states-reference-lightTimeCorrected'), 'WGC_StateVector'),
     append,
     buildStateTestCaseRunner({ corrections: [CorrectionType.LightTime] }),
+    runState2TestCases,
     'State with light time correction applied'
   );
   await testSuite(findFiles(path.join(__dirname, 'data/states-reference-starAberrationCorrected'), 'WGC_StateVector'),
     append,
     buildStateTestCaseRunner({ corrections: [CorrectionType.LightTime, CorrectionType.StarAbberation] }),
+    runState2TestCases,
     'State with star aberration and light time correction applied'
-  );
-
-  await testSuite(findFiles(path.join(__dirname, 'data/states-reference-uncorrected'), 'WGC_StateVector'),
-    append,
-    runState2TestCases,
-    'State without correction. New State Solver.'
-  );
-  await testSuite(findFiles(path.join(__dirname, 'data/states-reference-lightTimeCorrected'), 'WGC_StateVector'),
-    append,
-    runState2TestCases,
-    'State with light time correction applied. New State Solver.'
-  );
-  await testSuite(findFiles(path.join(__dirname, 'data/states-reference-starAberrationCorrected'), 'WGC_StateVector'),
-    append,
-    runState2TestCases,
-    'State with star aberration and light time correction applied. New State Solver.'
   );
 
   flush();
