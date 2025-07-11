3157f36b7d1fcb65fcdae3eaf5c8a091915c7fe5
Jakub Przybytek <jakub.przybytek@vml.com>
2025-07-11 18:21:32 +0200

    Skeleton and logo link fix

--- packages/web/src/components/Events/EventsList.tsx
+++ packages/web/src/components/Events/EventsList.tsx
@@ -4,6 +4,7 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import { type Event } from './EventsBrowser';
 import DisplayEvent from './event/DisplayEvent';
+import { Skeleton, Stack } from '@mui/material';
 
 type EventsListPropsType = {
   events: Event[];
@@ -11,6 +12,16 @@ type EventsListPropsType = {
 
 export default function EventsList({ events }: EventsListPropsType): JSX.Element {
   const theme = useTheme();
+
+  if (events.length === 0) {
+    return (
+      <Stack spacing={1}>
+        <Skeleton variant="rounded" animation="wave" width="100%" height={70} />
+        <Skeleton variant="rounded" animation="wave" width="100%" height={70} />
+        <Skeleton variant="rounded" animation="wave" width="100%" height={70} />
+      </Stack>
+    );
+  }
 
   return (
     <Box>
