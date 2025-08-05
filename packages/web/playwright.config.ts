31baf4faab371b14976be3f318f4205fd314061b
Jakub Przybytek <jakub.przybytek@vml.com>
2025-08-05 09:13:39 +0200

    Another fix 3

--- packages/web/playwright.config.ts
+++ packages/web/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.TEST_BASE_URL || 'http://localhost:5173',
+    baseURL: 'https://albedoonline.com',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     /* Action timeout (for page.click, page.fill, etc.) */
@@ -49,11 +49,4 @@ export default defineConfig({
       dependencies: ['setup'],
     },
   ],
-
-  /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:5173',
-    reuseExistingServer: !process.env.CI,
-  },
 });
