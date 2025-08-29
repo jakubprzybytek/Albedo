78d10c8491fff3974ce4dceb918a2c934865ed05
Jakub Przybytek <jakub.przybytek@gmail.com>
2025-08-29 08:27:29 +0200

    Test

--- packages/core/src/jpl/state/solver/__tests__/StateSolver.frame.test.ts
+++ packages/core/src/jpl/state/solver/__tests__/StateSolver.frame.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { EphemerisSeconds, JplBodyId, Vector3 } from '@jpl';
+import { EphemerisSeconds, JplBodyId, Matrix3x3, Vector3 } from '@jpl';
 import { kernels } from "@jpl/data/kernels.testData";
 import { CorrectionType } from "@jpl/state";
 import { RotationMatrix } from "@jpl/frames/RotationMatrix";
@@ -28,6 +28,22 @@ describe("StateSolver", () => {
     expect(bodyFixedPosition[0]).approximately(240699.17507517, 2e-6);
     expect(bodyFixedPosition[1]).approximately(-302343.65321879, 2e-6);
     expect(bodyFixedPosition[2]).approximately(-119223.00733439, 1e-6);
+  });
+
+  it("should correctly compute state for Moon wrt. Earth using WeGeocalc data", () => {
+    const position: Vector3 = [317255.79483133, -220341.79779477, -119833.86746624];
+
+    const rotationMatrix: Matrix3x3 = [
+      [0.95774664, 0.28760734, -0.00183938],
+      [-0.28760680, 0.95774841, 0.00055679],
+      [0.00192180, -4.25031809E-06, 0.99999815]
+    ];
+
+    const bodyFixedPosition = RotationMatrix.multiplyVector(rotationMatrix, position);
+
+    expect(bodyFixedPosition[0]).approximately(240699.17507517, 3e-3);
+    expect(bodyFixedPosition[1]).approximately(-302343.65321879, 7e-3);
+    expect(bodyFixedPosition[2]).approximately(-119223.00733439, 3e-4);
   });
 
 });
