d758bb76f1d462c730556a9183cae402edf34e81
Jakub Przybytek <jakub.przybytek@gmail.com>
2025-09-01 22:59:16 +0200

    Test fixed

--- packages/core/src/astro/scripts/paralaxCorrection/__tests__/ParalaxCorrection.test.ts
+++ packages/core/src/astro/scripts/paralaxCorrection/__tests__/ParalaxCorrection.test.ts
@@ -6,37 +6,37 @@ import { ParalaxCorrection } from "../ParalaxCorrection";
 import { CorrectionType } from "@jpl/state";
 import { States } from "@astro/scripts/states";
 
-describe.skip("paralaxCorrections", () => {
+describe("paralaxCorrections", () => {
   const paralaxCorrections = new ParalaxCorrection(kernels);
   const states = new States(kernels.stateSolver());
 
   it("should compute Sun distance from azimuth for 0°N, 0°E", () => {
-    const es = EphemerisSeconds.fromDateTime(2019, 10, 9, 12, 0, 17);
+    const es = EphemerisSeconds.fromDateTime(2019, 10, 9, 11, 47, 18);
 
     const observerPosition = paralaxCorrections.observerPosition(0, 0, 0, es);
     const sunPostion = states.position(JplBodyId.Sun, JplBodyId.Earth, es, CorrectionType.NONE);
     const separationDec = Radians.toDegrees(Radians.between(observerPosition, sunPostion));
 
-    expect(separationDec).toBeCloseTo(90 - Radians.toDegrees(Radians.fromDegrees2(81, 33, 44.2)), 1);
+    expect(separationDec).toBeCloseTo(90 - Radians.toDegrees(Radians.fromDegrees2(83, 44, 56.1)), 1);
   });
 
   it("should compute Sun distance from azimuth 0°N, 90°E", () => {
-    const es = EphemerisSeconds.fromDateTime(2025, 8, 31, 6, 0, 22);
+    const es = EphemerisSeconds.fromDateTime(2019, 10, 9, 5, 47, 23);
 
     const observerPosition = paralaxCorrections.observerPosition(90, 0, 0, es);
     const sunPostion = states.position(JplBodyId.Sun, JplBodyId.Earth, es, CorrectionType.NONE);
     const separationDec = Radians.toDegrees(Radians.between(observerPosition, sunPostion));
 
-    expect(separationDec).toBeCloseTo(90 - Radians.toDegrees(Radians.fromDegrees2(81, 28, 19.3)), 1);
+    expect(separationDec).toBeCloseTo(90 - Radians.toDegrees(Radians.fromDegrees2(83, 50, 38.1)), 1);
   });
 
   it("should compute Sun distance from azimuth 45°N, 90°E", () => {
-    const es = EphemerisSeconds.fromDateTime(2025, 8, 31, 6, 0, 24);
+    const es = EphemerisSeconds.fromDateTime(2019, 10, 9, 5, 47, 8);
 
     const observerPosition = paralaxCorrections.observerPosition(90, 45, 0, es);
     const sunPostion = states.position(JplBodyId.Sun, JplBodyId.Earth, es, CorrectionType.NONE);
     const separationDec = Radians.toDegrees(Radians.between(observerPosition, sunPostion));
 
-    expect(separationDec).toBeCloseTo(90 - Radians.toDegrees(Radians.fromDegrees2(53, 32, 27.7)), 0);
+    expect(separationDec).toBeCloseTo(90 - Radians.toDegrees(Radians.fromDegrees2(38, 51, 40.2)), 0);
   });
 });
